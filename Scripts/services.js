e8641bee04bdcd055fabc6b96a3454b122161c76
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-09-25 22:42:11 +0530

    final added

--- Scripts/services.js
+++ Scripts/services.js
@@ -1,31 +1,26 @@
 function validate() {
     var phoneInput = document.ownerDetailsForm.phone.value;
-    if(phoneInput.length<10){
-        document.getElementById("errorPhone").innerHTML=" Enter a Valid number";
-        document.ownerDetailsForm.phoneInput.focus();
-        return false;
-    }
-}
-
-
-function valEmail(){
     var emailInput = document.ownerDetailsForm.email.value;
     if (emailInput.indexOf("@") <= 0) {
         document.getElementById("errorEmail").innerHTML = "Enter a valid Email";
     }
-    validate();
-}
 
-
-
-function reset() {
-    document.getElementById("nameField").innerHTML=" "
-    document.getElementById("phoneInput").innerHTML=" "
-    document.getElementById("emailInput").innerHTML=" "
-
+    if(phoneInput.length<10){
+        document.getElementById("errorPhone").innerHTML=" Enter a Valid number";
+        return false;
+    } else {
+        submitted();
+    }
 }
 
 
 function submitted() {
-    alert("Hi! "+ document.ownerDetailsForm.nameField.value + ", We will get back you soon");
+    alert("Hi! "+ document.ownerDetailsForm.name.value + ", We will get back you soon");
+}
+
+
+function reset() {
+    document.getElementById("name").innerHTML=" "
+    document.getElementById("phone").innerHTML=" "
+    document.getElementById("email").innerHTML=" "
 }
