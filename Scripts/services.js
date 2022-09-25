fefb043cf6dcd9df242aa393f275fcb5112793a5
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-09-25 20:36:30 +0530

    button added

--- Scripts/services.js
+++ Scripts/services.js
@@ -1,63 +1,63 @@
-const name = document.getElementById("nameField")
-const email = document.getElementById("emailInput")
-const phone = document.getElementById("phoneInput")
-const form = document.getElementById("ownerDetailsForm")
+// const name = document.getElementById("nameField");
+// const email = document.getElementById("emailInput");
+// const phone = document.getElementById("phoneInput");
+// const form = document.getElementById("ownerDetailsForm");
 
 
 
 
-form.addEventListener('submit',e=>{
-    e.preventDefault();
-    checkInput();
-})
+// form.addEventListener('submit',e=>{
+//     e.preventDefault();
+//     checkInput();
+// })
 
 
-function checkInput() {
-    const emailVal = email.value.trim()
-    const phoneVal = phone.value.trim()
-    const emailval = email.value.trim()
+// function checkInput() {
+//     const emailVal = email.value.trim()
+//     const phoneVal = phone.value.trim()
+//     const emailval = email.value.trim()
 
 
-    if(emailVal === ''){
-        setError(email,"Email canot be blank")
-    }
-
-    else if (!isEmail(emailVal)) {
-        setError(email,"Not a valid mail")
-    }
-
-    else{
-        setSuccess(mail)
-    }
-
-}
-
-
-function isEmail(email){
-    return "@"
-
-}
-
-// function validate() {
-//     if(document.ownerDetailsForm.phoneInput.value.length<10){
-//         document.getElementById("phoneInput").innerHTML=" Enter a Valid number"
-//         document.ownerDetailsForm.phoneInput.foucs()
-//         return false
+//     if(emailVal === ''){
+//         setError(email,"Email canot be blank")
 //     }
+
+//     else if (!isEmail(emailVal)) {
+//         setError(email,"Not a valid mail")
+//     }
+
+//     else{
+//         setSuccess(mail)
+//     }
+
 // }
 
 
-// function valEmail(){
-//     var emailInput = document.ownerDetailsForm.emailInput.value
-//     if (emailInput.indexof("@")<=0) {
-//         // document.getElementById("emailInput").innerHTML = "Enter a valid Email"
-//         alert("fail")
-//         return false
+// function isEmail(email){
+//     return "@"
 
-//     }
-
-//     validate()
 // }
+
+function validate() {
+    alert("VAL")
+    if(document.ownerDetailsForm.phoneInput.value.length<10){
+        document.getElementById("phoneInput").innerHTML=" Enter a Valid number";
+        document.ownerDetailsForm.phoneInput.foucs();
+        return false;
+    }
+}
+
+
+function valEmail(){
+    const emailInput = document.ownerDetailsForm.emailInput.value;
+    alert("TEST")
+    if (emailInput.indexof("@")<=0) {
+        document.getElementById("emailInput").innerHTML = "Enter a valid Email";
+        alert("fail")
+        return false;
+    }
+    validate();
+}
 
 
 
@@ -73,6 +73,6 @@ function reset() {
 }
 
 
-// function submitted() {
-//     alert("Hi! "+ document.ownerDetailsForm.nameField.value + ", We will get back you soon")
-// }
+function submitted() {
+    alert("Hi! "+ document.ownerDetailsForm.nameField.value + ", We will get back you soon");
+}
